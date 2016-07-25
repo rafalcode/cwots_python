20186330fea0eb80b3e2c98bb1f804d333b6e9d6
Ramon Fallon <ramonfallon@gmail.com>
2016-07-25 23:37:35 +0100

    thread example

---
+++ thre0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+import threading
+import time
+
+class count_stuff(threading.Thread):
+    """
+        A thread class that will count a number, sleep and output that number
+    """
+    def __init__ (self, start_num, end):
+        self.num = start_num
+        threading.Thread.__init__ (self)
+   
+    def run(self):
+        while True:
+            if self.num != end:
+                self.num += 1
+                print "Outputting: ", str(self.num)
+                time.wait(5)
+            else:
+                break
+       
+myThread = count_stuff(1, 5)
+myThread.start()
