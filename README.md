e2e942b98e156f61644c03f65311309870ebb3d6
Ramon Fallon <ramonfallon@gmail.com>
2016-08-01 15:30:07 +0100

    wlk2.py cwd fastq

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 these are (usually) simple python scripts whose purpose is demonstrate
 the ways certain thigns can be done in this language.
+
+
+# wlk2.py
+captures all fastq.gz files in current directoryodoes some parsing on their names
--- wlk2.py
+++ wlk2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 # Getting to know os.walk
 import os, regex, sys
+from collections import deque
 
 argquan=len(sys.argv)
 if argquan != 2:
@@ -13,7 +14,8 @@ if argquan != 2:
 # fnl: list of filenames in CWD
 COU=0
 DLL={}
-PYRGX=regex.compile(r'.+\.py')
+# PYRGX=regex.compile(r'.+\.py')
+FQRGX=regex.compile(r'(([^_]+_)+(\w+))\.fastq\.gz')
 
 for (cwdletl, subdl, fnl) in os.walk(sys.argv[1]):
     rdp="".join(cwdletl) # real dp.
@@ -23,9 +25,20 @@ for (cwdletl, subdl, fnl) in os.walk(sys.argv[1]):
 
 for k in DLL:
     print "%s: " %k,
-    # for j in k:
-        # print j,
-
+    # the second list hold the files in CWD
+    cou=0
+    for j in DLL[k][1]:
+        m=FQRGX.match(j)
+        if m:
+            cou+=1
+            # mg=m.groups()
+            # for i in mg:
+            #     print "%s " % i,
+            lg=m.captures(2)
+            for i in lg:
+                print "%s " % i,
+            print
+print cou
         # for i in j:
         #     if PYRGX.match(i):
         #         print i,
