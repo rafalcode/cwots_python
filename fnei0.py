b7c6ac53bae51c10045e66a75e5cd607bae49cb4
nutria <nutria@aspes.eastc>
2017-01-25 21:46:49 +0000

    debia's emboss doesn't have fnei

--- fnei0.py
+++ fnei0.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python2
-# from Bio.Emboss.Applications import NeedleCommandline
+from Bio.Emboss.Applications import NeedleCommandline
 from Bio.Emboss.Applications import FNeighborCommandline
+
+needle_cline = NeedleCommandline(asequence="a0.fa", bsequence="b0.fa", gapopen=10, gapextend=0.5, outfile="needle.txt")
+print type(needle_cline)
+# print(needle_cline)
+# needle -outfile=needle.txt -asequence=alpha.faa -bsequence=beta.faa -gapopen=10 -gapextend=0.5
+print dir(needle_cline)
+# so how to execute? simple
+# sin,ser=needle_cline()
+# here we capture stdin and sterr which can be checked for longer programs.
+
+# Try the fnei
+neicmdl=FNeighborCommandline(datafile="neighbor.dat", outfile="fneiout.txt")
+print dir(neicmdl)
+sin,ser=neicmdl()
+print sin
+print ser
