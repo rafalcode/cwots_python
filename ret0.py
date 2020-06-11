c8902b927a906f0fccb912dca5f2057ef59500ef
rafalcode <ramonfallon@gmail.com>
2020-06-11 16:36:56 +0100

    checkign out what normal functons can return

---
+++ ret0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import math
+import os
+import sys
+
+# Complete the miniMaxSum function below.
+def anyf0():
+    what="John Keats"
+    return what
+
+def anyf2():
+    what=["Percy Shelley", "Lord Byron", "William W"]
+    return 3, what[1:3]
+
+if __name__ == '__main__':
+    # st0=anyf0()
+    # print(type(st0))
+    # print(len(st0))
+    # print(F"Hellp {st0}")
+    st2=anyf2()
+    print(len(st2))
+    print(type(st2))
