a722a2127992395bbf6d366facf7af8a34a69fb2
rafalco <ramonfallon@gmail.com>
2020-12-14 14:49:33 +0000

    some refinements to rg2, had to use replace to sko a matched character, I should know. I'm a regex wizard

--- rg2.py
+++ rg2.py
@@ -14,10 +14,19 @@ if argquan != 2:
 # for the days thing
 # RGX=regex.compile(r'^(?P<day>[a-zA-Z]+) (?P<time>[^\t]+)\t(?P<lat>[^\t]+)\t(?P<lng>[^\t]+)')
 RGX=regex.compile(r'^(?P<f3>[^ ]{,3})[^ ]* +(?P<s2>[^ ]{,2}).+')
+RGX2=regex.compile(r'^(?P<f3>[^ ]{,3})[^ ]* +[^ ]+ +(?P<s2>[^ ]{,2}).+')
 # RGX=regex.compile(r'^(?P<f3>[^ ]+) (?P<s2>[^ ]*).\+')
 # RGX=regex.compile(r'(?P<f3>[^ ]+) .+')
-name = sys.argv[1]
-m=RGX.match(name)
+name0 = sys.argv[1]
+c=name0.count(" ")
+print("space-count: "+str(c))
+name=name0.replace("'", "")
+
+if c == 1:
+    m=RGX.match(name)
+elif c == 2:
+    m=RGX2.match(name)
+
 if m is not None:
     gd = m.groupdict()
     outs = gd['f3']+gd['s2']
