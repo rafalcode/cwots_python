0c9b266cc929fba93bb0f3f57857203ce6c7e639
Ramon Fallon <ramonfallon@gmail.com>
2022-02-02 00:31:28 +0000

    minor change or simple comment

--- seabver0.py
+++ seabver0.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-# using Seaborn v 0.9,0
+# Seaborn is the scientific visualisation library built on matplotlib
+# It has  very handy annotate() function for adding text boes on a plot (most often JointGrid)
+# but for some reason (maybe it was inefficient) it did away with an wanted users to use ax_joint.text instead.
+# so this is proof concept that it does work, for both early and latest versions of seaborn.
+
 import sys 
 import seaborn as sns
 from scipy import stats
@@ -18,8 +22,8 @@ g = g.plot_marginals(sns.distplot, kde=False, color="g")
 # scipy's stats.pearsonr will be used here. It returns 2 floats: the coefficient first and then the p-value.
 # g = g.annotate(stats.pearsonr)
 # g = g.annotate(stats.pearsonr, template="{stat}: {val:.4f}", stat="Pearson's r", loc="upper left", fontsize=15)
-pearr= stats.pearsonr(tips.total_bill, tips.tip)
-st=f"Pearson's r = {pearr[0]:.4f}"
-# g.ax_joint.text(3.5, 10.0, st, fontsize=15)
+pearr= stats.pearsonr(tips.total_bill, tips.tip) # precalc
+st=f"Pearson's r = {pearr[0]:.4f}" # stringify
+# g.ax_joint.text(3.5, 10.0, st, fontsize=15) # na, not that easy .. add in the extra "chweing in order to be able to use 0.05, 0.95
 g.ax_joint.text(0.05, 0.95, st, fontsize=15, transform=g.ax_joint.transAxes, verticalalignment='top')
 g.savefig("d09.pdf", format="pdf")
